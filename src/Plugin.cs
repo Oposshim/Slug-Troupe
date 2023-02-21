2de2848b8a9ff45761ee8a5425b58572aae97f6f
Poss <63132454+Oposshim@users.noreply.github.com>
2023-02-20 20:44:23 -1100

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using System.Security.Permissions;
+
+#pragma warning disable CS0618 // Do not remove the following line.
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace SunHat;
+
+[BepInPlugin("Slug-Troupe", "slugtroupe", "1.0.0")]
+sealed class Plugin : BaseUnityPlugin
+{
+    FAtlas atlas;
+
+    public void OnEnable()
+    {
+        On.RainWorld.OnModsInit += Init;
+        On.PlayerGraphics.DrawSprites += PlayerGraphics_DrawSprites;
+    }
+
+    private void Init(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+
+        atlas ??= Futile.atlasManager.LoadAtlas("sprites/EXtemplatehead");
+
+        if (atlas == null) {
+            Logger.LogWarning("The clowns cannot found. Please reinstall the mod. co:");
+        }
+    }
+
+    private void PlayerGraphics_DrawSprites(On.PlayerGraphics.orig_DrawSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, UnityEngine.Vector2 camPos)
+    {
+        orig(self, sLeaser, rCam, timeStacker, camPos);
+
+        if (atlas == null) {
+            return;
+        }
+
+        string name = sLeaser.sprites[3]?.element?.name;
+        if (name != null && name.StartsWith("HeadA") && atlas._elementsByName.TryGetValue("EXtemplate" + name, out var element)) {
+            sLeaser.sprites[3].element = element;
+        }
+    }
+}
